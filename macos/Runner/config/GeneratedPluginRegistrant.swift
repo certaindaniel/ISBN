d42aac073d6deb83a1b1b6d46809271deb8827d1
Daniel.lu <Daniel.lu@bionime.com>
2025-12-17 16:39:27 +0800

    chore(platform): 新增 iOS/Android/macOS 平台專案檔
    Why: 支援多平台建置與執行。
    What:
    - iOS/Android/macOS 專案與設定檔
    Files:
    - ios/*
    - android/*
    - macos/*

---
+++ macos/Runner/config/GeneratedPluginRegistrant.swift
@@ -0,0 +1,5 @@
+info_plist_values = {
+  'NSCameraUsageDescription': 'The app needs camera access to scan book ISBN codes',
+  'NSMicrophoneUsageDescription': 'Microphone access may be required for barcode scanning',
+  'NSPhotoLibraryUsageDescription': 'Allow selecting book cover images from photo library',
+}
